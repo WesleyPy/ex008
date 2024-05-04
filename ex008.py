a9617e8b6bb523cbc26480385e74c01188f0173a
WesleyPy <usuariopc835@gmail.com>
2024-05-04 01:41:44 -0300

    Create ex008.py

---
+++ ex008.py
@@ -0,0 +1,8 @@
+d = float(input('Digite uma distância em metros: '))
+c = d * 100
+m = d * 1000
+print('A medida de {}m corresponde a:'.format(d))
+print('{}cm'.format(c))
+print('{}mm'.format(m))
+
+
